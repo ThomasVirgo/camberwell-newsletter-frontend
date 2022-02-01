bb9e0aaab437448a0cd1a52dd069e15aacf23ed4
Tom Virgo <tomcvirgo@gmail.com>
2022-02-01 16:36:07 +0000

    allow users to add a new post

--- src/lib/requests_posts.js
+++ src/lib/requests_posts.js
@@ -2,16 +2,6 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:8000/api/models'
 
-async function createPost(payload){
-    try {
-        const {data} = await axios.post(`${BASE_URL}/posts/`, payload, {
-            headers: tokenHeader()
-        })
-        return [data, false]
-    } catch (error) {
-        return [error.response, true]
-    }
-}
 
 async function getPosts(){
     try {
@@ -28,6 +18,20 @@ async function getCommentsForPost(post_id){
     try {
         const {data} = await axios.get(`${BASE_URL}/post_comments/${post_id}/comments_on_post/`, {
             headers: tokenHeader()
+        })
+        return [data, false]
+    } catch (error) {
+        return [error.response, true]
+    }
+}
+
+async function createPost(formData){
+    try {
+        const {data} = await axios.post(`${BASE_URL}/posts/add_post_by_email/`, formData, {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem('token')}`,
+                "content-type": "multipart/form-data"
+            }
         })
         return [data, false]
     } catch (error) {
--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { getPosts } from "../../lib/requests_posts";
+import { getPosts, createPost } from "../../lib/requests_posts";
 import { Post } from "../../components";
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
+    const [isFormShowing, setIsFormShowing] = useState(false)
+    const [postInput, setPostInput] = useState({
+        "title": "",
+        "content": "",
+        "image": null
+    })
+    
     
     useEffect(()=>{
         async function requestPosts(){
@@ -17,10 +24,57 @@ const Posts = () => {
         requestPosts()
     }, [])
 
+    async function handleSubmit(event){
+        event.preventDefault()
+        console.log(postInput);
+        let formData = new FormData();
+        formData.append('image', postInput.image, postInput.image.name);
+        formData.append('title', postInput.title);
+        formData.append('content', postInput.content);
+        formData.append('author', JSON.parse(localStorage.getItem('authData')).email)
+        let [data, isError] = await createPost(formData)
+        console.log(data);
+    }
+
+    function handleChange(event){
+        if (event.target.name === 'image'){
+            let newInput = {
+                ...postInput,
+                "image": event.target.files[0]
+            }
+            setPostInput(newInput)
+            return
+        }
+        let newInput = {...postInput}
+        newInput[event.target.name] = event.target.value
+        setPostInput(newInput)
+    }
+
+    function toggleForm(){
+        setIsFormShowing(prev => !prev)
+    }
+
     const postElements = posts.map((p, idx) => <div key={idx}><Post info={p}/></div>)
     return (
         <>
         <h1>Posts</h1>
+        <button onClick={toggleForm}>Add new post +</button>
+
+        { isFormShowing &&
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder="title" name='title' onChange={handleChange}></input>
+            <textarea name="content" rows="4" cols="50" onChange={handleChange}></textarea>
+            <input type='file' name='image' onChange={handleChange}></input>
+            <input type='submit' value='Submit'></input>
+        </form>}
+
+        {postInput.image && isFormShowing && 
+        <div>
+            <img alt="not fount" width={"250px"} src={URL.createObjectURL(postInput.image)} />
+            <br />
+            <button onClick={()=>setPostInput({...postInput, "image": null})}>Remove</button>
+        </div>}
+
         {postElements}
         </>
     )
