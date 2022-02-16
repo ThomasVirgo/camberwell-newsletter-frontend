b907634adc5d4efd5980c050ad898d091e29572c
Tom Virgo <tomcvirgo@gmail.com>
2022-02-16 11:23:35 +0000

    add responsive card posts with tailwind

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -32,15 +32,31 @@ const Post = ({info}) => {
     let commentElements = comments.map((c, idx) => <div key={idx}><Comment data={c}></Comment></div>)
 
     return (
-        <>
-        <h1>{info.title}</h1>
-        <p>{info.content}</p>
-        <img src={img_url} alt=""/>
-        <button onClick = {toggleComments}>{areCommentsShowing ? 'Hide' : 'Show'} Comments</button>
-        {areCommentsShowing && commentElements}
-        <CommentForm post_id = {info.id}></CommentForm>
-        </>
-    )
+        <div className="relative bg-white py-6 px-6 rounded-3xl w-80 h-80 my-4 shadow-xl">
+            <div className="mt-8">
+                <p className="text-lg font-semibold my-2">{info.title}</p>
+                <div>
+                    <img class="object-scale-down h-48 w-96" src={img_url}/>
+                </div>
+                <div className="flex justify-between">
+                    <div className="my-2">
+                        <p className="font-semibold text-base mb-2">Author</p>
+                    </div>
+                     <div className="my-2">
+                        <p className="font-semibold text-gray-400 mb-2">Date</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+        {/* <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 mb-4">
+            <h1>{info.title}</h1>
+            <p>{info.content}</p>
+            <img src={img_url} alt=""/>
+            <button onClick = {toggleComments}>{areCommentsShowing ? 'Hide' : 'Show'} Comments</button>
+            {areCommentsShowing && commentElements}
+            <CommentForm post_id = {info.id}></CommentForm>
+        </div> */}
 }
 
 export default Post
--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -74,8 +74,12 @@ const Posts = () => {
             <br />
             <button onClick={()=>setPostInput({...postInput, "image": null})}>Remove</button>
         </div>}
-
-        {postElements}
+        
+        <div class="flex items-center justify-center">
+            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+                {postElements}
+            </div>
+        </div>
         </>
     )
 }
